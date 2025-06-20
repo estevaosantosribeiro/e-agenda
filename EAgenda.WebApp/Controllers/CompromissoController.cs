c2397bacaa1a5db15f962183c399e65a68df3acf
LeandroJader <leziin001@gmail.com>
2025-06-20 13:39:09 -0300

    implementada funcionalidade de exclusão de compromissos

--- EAgenda.WebApp/Controllers/CompromissoController.cs
+++ EAgenda.WebApp/Controllers/CompromissoController.cs
@@ -5,66 +5,85 @@ using EAgenda.Infraestrutura.Arquivos.ModuloCompromisso;
 using EAgenda.WebApp.Extensions;
 using EAgenda.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using static EAgenda.WebApp.Models.DetalhesCompromissoViewModel;
 
-namespace EAgenda.WebApp.Controllers
+namespace EAgenda.WebApp.Controllers;
+
+[Route("compromissos")]
+public class CompromissoController : Controller
 {
-    [Route("compromissos")]
-    public class CompromissoController : Controller
+    private readonly IrepositorioCompromisso repositorio;
+    private readonly ContextoDados contextodados;
+    public CompromissoController()
     {
-        private readonly IrepositorioCompromisso repositorio;
-        private readonly ContextoDados contextodados;
-        public CompromissoController()
-        {
-            contextodados = new ContextoDados(true);
-            repositorio = new RepositorioCompromisso(contextodados);
-        }
-
-        public IActionResult Index()
-        {
-            var registroSelecionado = repositorio.SelecionarRegistros();
-            var visualizarVM = new VisualizarCompromissoViewModel(registroSelecionado);
-
-            return View(visualizarVM);
-        }
-        [HttpGet("cadastrar")]
-        public IActionResult Cadastrar()
-        {
-            var cadastrarVM = new CadastrarCompromissoViewModel();
-
-            return View(cadastrarVM);
-        }
-
-        [HttpPost("cadastrar")]
-
-        public IActionResult Cadastrar(CadastrarCompromissoViewModel cadastrarVm)
-        {
-            var entidade = cadastrarVm.ParaEntidade();
-            repositorio.CadastrarRegistro(entidade);
-
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet("editar/{Id:Guid}")]
-
-        public IActionResult Editar(Guid id)
-        {
-            var registroSelecionado = repositorio.SelecionarRegistroPorId(id);
-
-            var editarVM = new EditarCompromissoViewModel(registroSelecionado.Id, registroSelecionado.Assunto, registroSelecionado.DataDeOcorrencia, registroSelecionado.HoraDeInicio, registroSelecionado.HoraDeTermino, registroSelecionado.TipoDeOcorrido);
-
-           return View(editarVM);
-        }
-        [HttpPost("editar/{Id:Guid}")]
-
-        public IActionResult Editar(Guid id, EditarCompromissoViewModel editarVM)
-        {
-            var entidade = editarVM.ParaEntidade();
-
-            repositorio.EditarRegistro(id, entidade);
-
-            return RedirectToAction(nameof(Index));
-        }
+        contextodados = new ContextoDados(true);
+        repositorio = new RepositorioCompromisso(contextodados);
     }
 
-    
+    public IActionResult Index()
+    {
+        var registroSelecionado = repositorio.SelecionarRegistros();
+        var visualizarVM = new VisualizarCompromissoViewModel(registroSelecionado);
+
+        return View(visualizarVM);
+    }
+    [HttpGet("cadastrar")]
+    public IActionResult Cadastrar()
+    {
+        var cadastrarVM = new CadastrarCompromissoViewModel();
+
+        return View(cadastrarVM);
+    }
+
+    [HttpPost("cadastrar")]
+
+    public IActionResult Cadastrar(CadastrarCompromissoViewModel cadastrarVm)
+    {
+        var entidade = cadastrarVm.ParaEntidade();
+        repositorio.CadastrarRegistro(entidade);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("editar/{Id:guid}")]
+
+    public IActionResult Editar(Guid id)
+    {
+        var registroSelecionado = repositorio.SelecionarRegistroPorId(id);
+
+        var editarVM = new EditarCompromissoViewModel(registroSelecionado.Id, registroSelecionado.Assunto, registroSelecionado.DataDeOcorrencia, registroSelecionado.HoraDeInicio, registroSelecionado.HoraDeTermino, registroSelecionado.TipoDeOcorrido);
+
+        return View(editarVM);
+    }
+    [HttpPost("editar/{Id:guid}")]
+
+    public IActionResult Editar(Guid id, EditarCompromissoViewModel editarVM)
+    {
+        var entidade = editarVM.ParaEntidade();
+
+        repositorio.EditarRegistro(id, entidade);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("excluir/{Id:guid}")]
+
+    public IActionResult Excluir(Guid id)
+    {
+        var registroselecionado = repositorio.SelecionarRegistroPorId(id);
+
+        var excluirVm = new ExcluirCompromissoViewModel(registroselecionado.Id, registroselecionado.Assunto);
+
+        return View(excluirVm);
+    }
+
+    [HttpPost("excluir/{Id:guid}")]
+
+    public IActionResult ExcluirConfirmado(Guid Id,ExcluirCompromissoViewModel excluirVM)
+    {
+        repositorio.ExcluirRegistro(Id);
+
+        return RedirectToAction(nameof(Index));
+    }
 }
+
--- EAgenda.WebApp/Models/CompromissoViewModels.cs
+++ EAgenda.WebApp/Models/CompromissoViewModels.cs
@@ -6,27 +6,27 @@ using Microsoft.Win32;
 
 namespace EAgenda.WebApp.Models;
 
-    public class FormularioCompromissoViewModel
-    {
-        public string Assunto { get; set; }
-        public DateOnly DataDeOcorrencia { get; set; }
-        public  TimeSpan HoraDeInicio { get; set; }
-        public TimeSpan HoraDeTermino { get; set; }
-        public string TipoDeOcorrido { get; set; }
-    }   
-    public class CadastrarCompromissoViewModel : FormularioCompromissoViewModel
-    {
-        public CadastrarCompromissoViewModel() { }
+public class FormularioCompromissoViewModel
+{
+    public string Assunto { get; set; }
+    public DateOnly DataDeOcorrencia { get; set; }
+    public TimeSpan HoraDeInicio { get; set; }
+    public TimeSpan HoraDeTermino { get; set; }
+    public string TipoDeOcorrido { get; set; }
+}
+public class CadastrarCompromissoViewModel : FormularioCompromissoViewModel
+{
+    public CadastrarCompromissoViewModel() { }
 
-        public CadastrarCompromissoViewModel(string assunto, DateOnly dataDeOcorrencia, TimeSpan horaDeInico, TimeSpan horaDeTermino,string tipoDeOcorrido)  :this()
-        {
-            Assunto = assunto;
-            DataDeOcorrencia = dataDeOcorrencia;
-            HoraDeInicio = horaDeInico;
-            HoraDeTermino = horaDeTermino;
-            TipoDeOcorrido = tipoDeOcorrido;
-        }
+    public CadastrarCompromissoViewModel(string assunto, DateOnly dataDeOcorrencia, TimeSpan horaDeInico, TimeSpan horaDeTermino, string tipoDeOcorrido) : this()
+    {
+        Assunto = assunto;
+        DataDeOcorrencia = dataDeOcorrencia;
+        HoraDeInicio = horaDeInico;
+        HoraDeTermino = horaDeTermino;
+        TipoDeOcorrido = tipoDeOcorrido;
     }
+}
 public class EditarCompromissoViewModel : FormularioCompromissoViewModel
 {
     public Guid Id { get; set; }
@@ -34,49 +34,63 @@ public class EditarCompromissoViewModel : FormularioCompromissoViewModel
     public EditarCompromissoViewModel() { }
 
     public EditarCompromissoViewModel(Guid id, string assunto, DateOnly dataDeOcorrencia, TimeSpan horaDeInicio, TimeSpan horaDeTermino, string tipoDeOcorrido) : this()
-        {
-            Id = id;
-            Assunto = assunto;
-            DataDeOcorrencia = dataDeOcorrencia;
-            HoraDeInicio = horaDeInicio;
-            HoraDeTermino = horaDeTermino;
-            TipoDeOcorrido = tipoDeOcorrido;
-        }
-    }
-
-        public class VisualizarCompromissoViewModel
     {
-        public VisualizarCompromissoViewModel() { }
+        Id = id;
+        Assunto = assunto;
+        DataDeOcorrencia = dataDeOcorrencia;
+        HoraDeInicio = horaDeInicio;
+        HoraDeTermino = horaDeTermino;
+        TipoDeOcorrido = tipoDeOcorrido;
+    }
+}
 
-        public List<DetalhesCompromissoViewModel> Registros;
+public class VisualizarCompromissoViewModel
+{
+    public VisualizarCompromissoViewModel() { }
 
-        public VisualizarCompromissoViewModel(List<Compromisso> compromisso) 
-        {
-            Registros = new List<DetalhesCompromissoViewModel>();
+    public List<DetalhesCompromissoViewModel> Registros;
 
-            foreach (var c in compromisso)
+    public VisualizarCompromissoViewModel(List<Compromisso> compromisso)
+    {
+        Registros = new List<DetalhesCompromissoViewModel>();
+
+        foreach (var c in compromisso)
             Registros.Add(c.ParaDetalhesVm());
 
-        }
     }
-    public class DetalhesCompromissoViewModel
+}
+
+public class DetalhesCompromissoViewModel
+{
+    public Guid Id { get; set; }
+    public string Assunto { get; set; }
+    public DateOnly DataDeOcorrencia { get; set; }
+    public TimeSpan HoraDeInicio { get; set; }
+    public TimeSpan HoraDeTermino { get; set; }
+    public string TipoDeOcorrido { get; set; }
+
+
+    public DetalhesCompromissoViewModel(Guid id, string assunto, DateOnly dataDeOcorrencia, TimeSpan horaDeIncio, TimeSpan horaDeTermino, string tipodeocorrido)
+    {
+        Id = id;
+        Assunto = assunto;
+        DataDeOcorrencia = dataDeOcorrencia;
+        HoraDeInicio = horaDeIncio;
+        HoraDeTermino = horaDeTermino;
+        TipoDeOcorrido = tipodeocorrido;
+
+    }
+}
+    public class ExcluirCompromissoViewModel
     {
         public Guid Id { get; set; }
-        public string Assunto { get; set;}
-        public DateOnly DataDeOcorrencia { get; set;}
-        public TimeSpan HoraDeInicio { get; set;}
-        public TimeSpan HoraDeTermino { get; set;}
-        public string TipoDeOcorrido { get; set;}
+        public string Assunto { get; set; }
+        public ExcluirCompromissoViewModel() { }
 
-
-        public DetalhesCompromissoViewModel(Guid id ,string assunto, DateOnly dataDeOcorrencia, TimeSpan horaDeIncio, TimeSpan horaDeTermino, string tipodeocorrido)
+        public ExcluirCompromissoViewModel(Guid id, string assunto) : this()
         {
             Id = id;
             Assunto = assunto;
-            DataDeOcorrencia = dataDeOcorrencia;
-            HoraDeInicio = horaDeIncio;
-            HoraDeTermino = horaDeTermino;
-            TipoDeOcorrido = tipodeocorrido;
 
         }
     }
