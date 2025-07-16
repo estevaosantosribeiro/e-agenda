70fdf7eb082de1770e0912c5670a2342c6f298bc
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-07-16 00:07:04 -0300

    Implementado repositório base em orm

---
+++ EAgenda.Infraestrutura.Orm/Compartilhado/RepositorioBaseEmOrm.cs
@@ -0,0 +1,53 @@
+﻿using EAgenda.Dominio.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace EAgenda.Infraestrutura.Orm.Compartilhado;
+
+public class RepositorioBaseEmOrm<T> where T : EntidadeBase<T>
+{
+    private readonly DbSet<T> registros;
+
+    public RepositorioBaseEmOrm(EAgendaDbContext contexto)
+    {
+        this.registros = contexto.Set<T>();
+    }
+
+    public void CadastrarRegistro(T registro)
+    {
+        registros.Add(registro);
+    }
+
+    public bool EditarRegistro(Guid idRegistro, T registroEditado)
+    {
+        var registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+        if (registroSelecionado is null)
+            return false;
+
+        registroSelecionado.AtualizarRegistro(registroEditado);
+
+        return true;
+    }
+
+    public bool ExcluirRegistro(Guid idRegistro)
+    {
+        var registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+        if (registroSelecionado is null)
+            return false;
+
+        registros.Remove(registroSelecionado);
+
+        return true;
+    }
+
+    public T? SelecionarRegistroPorId(Guid idRegistro)
+    {
+        return registros.FirstOrDefault(x => x.Id.Equals(idRegistro));
+    }
+
+    public List<T> SelecionarRegistros()
+    {
+        return registros.ToList();
+    }
+}
